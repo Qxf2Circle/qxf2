592d51773c2613827e2c03070c70ea77ebe0ec0c
Qxf2Circle <testQxf2@gmail.com>
2015-09-29 15:54:23 +0530

    Update Selenium_Success_Test.py

--- test/Selenium_Success_Test.py
+++ test/Selenium_Success_Test.py
@@ -10,7 +10,7 @@ class Selenium_Success_Test(unittest.TestCase):
     def test_selenium_trial_page(self):
         driver = self.driver
         driver.get("http://qxf2.com/selenium-trial-main")
-        driver.save_screenshot('./test/Qxf2_Tutorial_success.png')
+        driver.save_screenshot('./screenshots/Qxf2_Tutorial_success.png')
         self.assertIn("Qxf2 Services: Selenium training main", driver.title)
         
 
