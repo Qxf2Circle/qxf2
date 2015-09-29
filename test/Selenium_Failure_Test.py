b2f0d0caa7c7d8c25214c2cb0cbd115eb88ab488
Qxf2Circle <testQxf2@gmail.com>
2015-09-29 15:53:31 +0530

    Update Selenium_Failure_Test.py

--- test/Selenium_Failure_Test.py
+++ test/Selenium_Failure_Test.py
@@ -10,9 +10,9 @@ class Selenium_Failure_Test(unittest.TestCase):
     def test_selenium_trial_page(self):
         driver = self.driver
         driver.get("http://qxf2.com/selenium-trial-main")
-        os.makedirs('./test/screenshot')
-        driver.save_screenshot('./test/screenshot/Qxf2_Tutorial_Failure.png')
-        self.assertIn("Qxf", driver.title)
+        os.makedirs('./screenshots')
+        driver.save_screenshot('./screenshot/Qxf2_Tutorial_Failure.png')
+        self.assertIn("Qxf2", driver.title)
         
 
     def tearDown(self):
